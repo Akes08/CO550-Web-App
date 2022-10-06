a0207be28b9d6b19bc4712cdf3b724272c5690da
Akes.Ali <ak5210365@gmail.com>
2022-10-06 14:46:03 +0100

    new razorpage

---
+++ First RazorPage/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace First_RazorPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Akes Ali";
+        }
+        
+        public void OnPost()
+        {
+            ViewData["Message"] = "Hello" + FullName;
+        }
+    }
+}
