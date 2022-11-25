08c9384ce92d1c3f58ebb6414ff0ff1100101597
Akes.Ali <ak5210365@gmail.com>
2022-11-25 19:00:18 +0000

    currency converter

--- RazorCurrencyConverter/Pages/Index.cshtml.cs
+++ RazorCurrencyConverter/Pages/Index.cshtml.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
 
 namespace RazorCurrencyConverter.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public string? x { get; set; }
+        public const double y = 1.21;
+        double result;
 
-        public void OnGet()
+        public void OnPost()
         {
+            if (!String.IsNullOrWhiteSpace(x))
+            {
+                result = Convert.ToDouble(x) * y;
+                ViewData["Message"] = $"£{x} Is ${result}";
+            }
 
         }
     }
