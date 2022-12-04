3a8a6ca05d026f21f69b1af765d11b1815c5c4e8
Akes.Ali <ak5210365@gmail.com>
2022-12-04 00:29:10 +0000

    contoso model and scaffolding

---
+++ ASP_RazorContoso/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP_RazorContoso.Data;
+using ASP_RazorContoso.Models;
+
+namespace ASP_RazorContoso.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ASP_RazorContoso.Data.ApplicationDbContext _context;
+
+        public DetailsModel(ASP_RazorContoso.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+      public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.StudentID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Student = student;
+            }
+            return Page();
+        }
+    }
+}
